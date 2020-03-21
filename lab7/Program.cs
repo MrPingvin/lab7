b9cd801dfd19aef8986a16bf359b71b2801abff2
MrPingvin <sgv.vsg1999@mail.ru>
2020-03-21 11:24:05 +0300

    Init

---
+++ lab7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    Console.WriteLine("Введите строку для сортировки");
+            char[] str = Console.ReadLine().ToCharArray();
+            Array.Sort(str);
+
+	    Console.WriteLine("Отсортированая строка");
+            Console.WriteLine(new String(str));
+            Console.ReadKey();
+        }
+    }
+}
